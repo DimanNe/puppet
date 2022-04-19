0b10edc6347e4ad3b52e20cf21d89740dad4941b
dimanne <dimanne@gmail.com>
2022-04-19 09:00:14 +0100

    apt

--- codedir/modules/envs/impedance.pp
+++ codedir/modules/envs/impedance.pp
@@ -1,9 +1,15 @@
-include libs::git::pkg
-include libs::sudo::enroll_eligible_users
-include libs::users::dimanne
+# include libs::git::pkg
+# include libs::sudo::enroll_eligible_users
+# include libs::users::dimanne
+
+# include stdlib
 
 # warning(User <| tag == sudo |>)
 
+class { libs::apt::sources_and_key:
+   gpg_bin_key_path => "/home/dimanne/devel/puppet/puppet-release-4528B6CD9E61EF26.pub.gpg",
+   sources_path => "/home/dimanne/devel/puppet/puppet-apt.sources",
+}
 
 # class { 'ntp':
 #    servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
--- codedir/modules/libs/manifests/apt/sources_and_key.pp
+++ codedir/modules/libs/manifests/apt/sources_and_key.pp
@@ -1,24 +1,34 @@
 class libs::apt::sources_and_key(
-   Stdlib::AbsolutePath sources_path,
-   Stdlib::AbsolutePath gpg_bin_key_path,
+   Stdlib::AbsolutePath $sources_path,
+   Stdlib::AbsolutePath $gpg_bin_key_path,
 ) {
-   $key_dir     = "/usr/share/keyrings/"
-   $sources_dir = "/etc/apt/sources.list.d/"
+   $key_dir     = "/usr/share/keyrings"
+   $sources_dir = "/etc/apt/sources.list.d"
 
-   file { "$key_dir"    : ensure => directory }
-   file { "$sources_dir": ensure => directory }
+   $sources_basename     = ::basename($sources_path)
+   $gpg_bin_key_basename = ::basename($gpg_bin_key_path)
 
-   file { Stdlib::basename(sources_path):
-      mode => "0644",
-      owner => 'root',
-      group => 'root',
-      source => $sources_path,
-   }
-   file { Stdlib::basename(gpg_bin_key_path):
-      mode => "0644",
-      owner => 'root',
-      group => 'root',
-      source => $gpg_bin_key_path,
-   }
+   $sources_dst_path     = "${sources_dir}/${sources_basename}"
+   $gpg_bin_key_dst_path = "${key_dir}/${gpg_bin_key_basename}"
+   
+   warning("${sources_path} new destination: ${sources_dst_path}")
+   warning("${gpg_bin_key_path} new destination: ${gpg_bin_key_dst_path}")
+
+#    file { "$key_dir"    : ensure => directory }
+#    file { "$sources_dir": ensure => directory }
+# 
+#    file { Stdlib::basename(sources_path):
+#       path => 
+#       mode => "0644",
+#       owner => 'root',
+#       group => 'root',
+#       source => $sources_path,
+#    }
+#    file { Stdlib::basename(gpg_bin_key_path):
+#       mode => "0644",
+#       owner => 'root',
+#       group => 'root',
+#       source => $gpg_bin_key_path,
+#    }
    # notify apt update
 }
