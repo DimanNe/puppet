e69735085dae2bf0d0f07d065207c1521c7d213a
dimanne <dimanne@gmail.com>
2022-06-04 09:35:09 +0100

    Change inotify tunables

--- codedir/modules/libs/manifests/tunables/inotify.pp
+++ codedir/modules/libs/manifests/tunables/inotify.pp
@@ -1,4 +1,5 @@
 class libs::tunables::inotify {
-   sysctl { 'kernel.perf_event_paranoid': value => '-1' }
-   sysctl { 'fs.inotify.max_user_watches': value => '16777216' }
+   sysctl { 'kernel.perf_event_paranoid'   : value => '-1' }
+   sysctl { 'fs.inotify.max_user_watches'  : value => '16777216' }
+   sysctl { 'fs.inotify.max_user_instances': value => '1048576' }
 }
